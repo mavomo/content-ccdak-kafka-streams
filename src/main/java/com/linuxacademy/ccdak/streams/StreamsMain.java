c1f309a0a97860e566de67566bb1483b738f0ae8
Michelle AVOMO <michelle.avomo.ext@loccitane.com>
2024-09-25 14:40:38 +0200

    feature: implement the basic Streams

--- src/main/java/com/linuxacademy/ccdak/streams/StreamsMain.java
+++ src/main/java/com/linuxacademy/ccdak/streams/StreamsMain.java
@@ -1,9 +1,49 @@
 package com.linuxacademy.ccdak.streams;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
 public class StreamsMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_DOC, 0);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        //Get the source stream
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, String> source = builder.stream("streams-input-topic");
+        source.to("streams-output-topic");
+
+        final Topology topology = builder.build();
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        System.out.println("Starting Kafka Streams " +  topology.describe());
+        final CountDownLatch latch = new CountDownLatch(1);
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-wordcount-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (final Throwable e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
     }
 
 }
